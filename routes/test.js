2decea3eaf60d53e2d6acf6e3033bf72750af5b8
Mimas <michal.maselko@awesome-ind.com>
2022-08-04 16:10:47 +0200

    .

--- routes/test.js
+++ routes/test.js
@@ -66,7 +66,7 @@ router.post('/test', async (req,res) =>{
    
     // const o = await Order.findById('61e1aff70af1be59793b6715');
     // console.log('O',o);
-    // await mongoose.disconnect();
+    await mongoose.disconnect();
 
 
     res.status(200).json({mail:'confirmed'});
