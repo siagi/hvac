5bd96a0b0464cd33cf8f5d2301faa814c6a6a145
Mimas <michal.maselko@awesome-ind.com>
2022-08-04 03:01:06 +0200

    .

--- google/sendEmail.js
+++ google/sendEmail.js
@@ -6,102 +6,104 @@ const nodemailer = require('nodemailer');
 const SCOPES = ['https://www.googleapis.com/auth/cloud-platform','https://mail.google.com/','https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/pubsub'];
 const TOKEN_PATH = 'token.json';
 
-fs.readFile('./credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Gmail API.
-    authorize(JSON.parse(content), sendEmail);
-  });
+async function sendGmailEmail(emailAddress) {
+    // fs.readFile('./credentials.json', (err, content) => {
+    //   if (err) return console.log('Error loading client secret file:', err);
+    //   // Authorize a client with credentials, then call the Gmail API.
+    //   authorize(JSON.parse(content), sendEmail, emailAddress);
+    // });
 
-
-  function authorize(credentials, callback, emailAddress) {
-    const {client_secret, client_id, redirect_uris} = credentials.web;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-  
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-      if (err) return getNewToken(oAuth2Client, callback);
-      oAuth2Client.setCredentials(JSON.parse(token));
-      // sendEmail(credentials,oAuth2Client);
-      callback({credentials, oAuth2Client, emailAddress});
-    });
-  }
-  
-  /**
-   * Get and store new token after prompting for user authorization, and then
-   * execute the given callback with the authorized OAuth2 client.
-   * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
-   * @param {getEventsCallback} callback The callback for the authorized client.
-   */
-  function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-      rl.close();
-      oAuth2Client.getToken(code, (err, token) => {
-        if (err) return console.error('Error retrieving access token', err);
-        oAuth2Client.setCredentials(token);
-        // Store the token to disk for later program executions
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-          if (err) return console.error(err);
-          console.log('Token stored to', TOKEN_PATH);
-        });
-        callback(oAuth2Client);
-      });
-    });
-  }
-
-
-  async function sendGmailEmail(emailAddress) {
     fs.readFile('./credentials.json', (err, content) => {
-      if (err) return console.log('Error loading client secret file:', err);
-      // Authorize a client with credentials, then call the Gmail API.
-      authorize(JSON.parse(content), sendEmail, emailAddress);
-    });
+        if (err) return console.log('Error loading client secret file:', err);
+        // Authorize a client with credentials, then call the Gmail API.
+        authorize(JSON.parse(content), sendEmail, emailAddress);
+      });
+    
+    
+      function authorize(credentials, callback, emailAddress) {
+        const {client_secret, client_id, redirect_uris} = credentials.web;
+        const oAuth2Client = new google.auth.OAuth2(
+            client_id, client_secret, redirect_uris[0]);
+      
+        // Check if we have previously stored a token.
+        fs.readFile(TOKEN_PATH, (err, token) => {
+          if (err) return getNewToken(oAuth2Client, callback);
+          oAuth2Client.setCredentials(JSON.parse(token));
+          // sendEmail(credentials,oAuth2Client);
+          callback({credentials, oAuth2Client, emailAddress});
+        });
+      }
+      
+      /**
+       * Get and store new token after prompting for user authorization, and then
+       * execute the given callback with the authorized OAuth2 client.
+       * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+       * @param {getEventsCallback} callback The callback for the authorized client.
+       */
+      function getNewToken(oAuth2Client, callback) {
+        const authUrl = oAuth2Client.generateAuthUrl({
+          access_type: 'offline',
+          scope: SCOPES,
+        });
+        console.log('Authorize this app by visiting this url:', authUrl);
+        const rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout,
+        });
+        rl.question('Enter the code from that page here: ', (code) => {
+          rl.close();
+          oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error retrieving access token', err);
+            oAuth2Client.setCredentials(token);
+            // Store the token to disk for later program executions
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+              if (err) return console.error(err);
+              console.log('Token stored to', TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+          });
+        });
+      }
+    
+      
+      /**
+       * Lists the labels in the user's account.
+       *
+       * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+       */
+    
+      async function sendEmail({credentials,oAuth2Client, emailAddress}){
+        // const gmail = google.gmail({version:'v1', auth});
+        const {client_secret, client_id, redirect_uris} = credentials.web;
+        try {
+          const accessToken = await oAuth2Client.getAccessToken();
+          const transport = nodemailer.createTransport({
+            service:'gmail',
+            auth:{
+              type:'OAuth2',
+              user:'zamowserwis24@gmail.com',
+              clientId:client_id,
+              clientSecret:client_secret,
+              accessToken: accessToken.token
+            },
+          })
+          const mailOptions = {
+            from:'ZamowSerwis24 <zamowserwis24@gmail.om>',
+            to: emailAddress,
+            subject:'New zamowserwis24',
+            text:'Hello from gmail email using API',
+            html: '<h1>Hello from gmail email using API</h1>'
+          } 
+    
+          const result = await transport.sendMail(mailOptions);
+          console.log('RESULT33',result)
+        } catch (error) {
+          console.log('error',error)
+        }
+      }
   }
-  
-  /**
-   * Lists the labels in the user's account.
-   *
-   * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-   */
 
-  async function sendEmail({credentials,oAuth2Client, emailAddress}){
-    // const gmail = google.gmail({version:'v1', auth});
-    const {client_secret, client_id, redirect_uris} = credentials.web;
-    try {
-      const accessToken = await oAuth2Client.getAccessToken();
-      const transport = nodemailer.createTransport({
-        service:'gmail',
-        auth:{
-          type:'OAuth2',
-          user:'zamowserwis24@gmail.com',
-          clientId:client_id,
-          clientSecret:client_secret,
-          accessToken: accessToken.token
-        },
-      })
-      const mailOptions = {
-        from:'ZamowSerwis24 <zamowserwis24@gmail.om>',
-        to: emailAddress,
-        subject:'New zamowserwis24',
-        text:'Hello from gmail email using API',
-        html: '<h1>Hello from gmail email using API</h1>'
-      } 
 
-      const result = await transport.sendMail(mailOptions);
-      console.log('RESULT33',result)
-    } catch (error) {
-      console.log('error',error)
-    }
-  }
 
 
 
