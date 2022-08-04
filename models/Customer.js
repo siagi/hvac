b12d9d791e4d1634e6ddb8b968dec64ee0d99bfa
Mimas <michal.maselko@awesome-ind.com>
2022-08-04 18:45:51 +0200

    .

--- models/Customer.js
+++ models/Customer.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const CustomerSchema = new mongoose.Schema(
     {
-        name:{type:String,required:false},
+        name:{type:String,required:false, unique:false},
         nip:{type:Number,unique:true},
         street:{type:String},
         postcode:{type:String},
--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
     {
-        username:{type:String,required:true,unique:true},
+        username:{type:String,required:true,unique:false},
         password:{type:String,required:true},
         name:{type:String,required:true},
         surname:{type:String,required:true},
