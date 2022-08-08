6c401e6db903a52b51cfe51aafd8eaee78c798f5
Mimas <michal.maselko@awesome-ind.com>
2022-08-08 08:31:25 +0200

    .

--- routes/test.js
+++ routes/test.js
@@ -81,130 +81,130 @@ function getNewGoogleToken(oAuth2Client, resolve) {
 
 router.post('/test', async (req,response) =>{
 
-    checkCredentials().then((e) => {
-        return new Promise((resolve, reject) => {
-            const gmail = google.gmail({version: 'v1',auth:e});
-            gmail.users.threads.list({
-              userId: 'me',
-            }, (err, res) => {
-              if (err) return console.log('The API returned an error: ' + err);
-              const messages = res.data.threads;
-              if (messages.length) {
-                for(let i=0; i<messages.length; i++){
-                    gmail.users.threads.get({
-                        userId:'me',
-                        id:messages[i].id
-                    }, (err,res)=>{
-                        if(err) console.log('err',err);
-                        const from = res.data.messages[0].payload.headers.find((h)=>h['name']==='From')
-                        const subject = res.data.messages[0].payload.headers.find((h)=>h['name']==='Subject');
-                        let text;
-                        if(res.data.messages[0].labelIds.includes('UNREAD')){
-                            const body = gmail.users.messages.get({
-                                userId:'me',
-                                id:messages[i].id
-                            })
-                            body.then(async (b)=> {
-                                // console.log('B',b.data.payload.parts[0].parts[0].body.data)
-                                console.log(b.data.payload.parts[0].hasOwnProperty('parts'))
-                                const data = b.data.payload.parts[0].hasOwnProperty('parts')  ? b.data.payload.parts[0].parts[0].body.data : b.data.payload.parts[0].body.data;
-                                text = new Buffer.from(data, 'base64').toString('utf-8');
-                                await mongoose.connect(process.env.MONGO_URL)
-                                .then(()=>console.log("DB connection Successfull"))
-                                .catch((error)=>console.log('MONGO ERROR',error));
-                                return new Promise(async (resolve, reject)=>{
-                                    let customer = await Customer.findOne({email:from.value});
-                                    if(!customer){
-                                        const newCustomer = new Customer({
-                                            email:from.value,
-                                            name:from.value,
-                                            nip:Date.now()
-                                        })
-                                        customer = await newCustomer.save();
-                                        console.log(customer)
-                                    }
-                                    const newOrder = new Order({
-                                        customer:customer._id,
-                                        description:text,
-                                        title:subject.value
-                                    })
-                                    const savedOrder = await newOrder.save();
-                                    console.log(savedOrder);
-                                    const orderID = savedOrder._id
-                                    resolve({from:from.value, orderID:orderID})
-                                }).then(async result => {
-                                    await new Promise(async (resolve, reject) => {
-                                        const { from, orderID } = result;
-                                        console.log('OAUTH22', oAuth2Client);
-                                        try {
-                                            const accessToken = await oAuth2Client.getAccessToken();
-                                            const transport = nodemailer.createTransport({
-                                                service: 'gmail',
-                                                auth: {
-                                                    type: 'OAuth2',
-                                                    user: 'zamowserwis24@gmail.com',
-                                                    clientId: oAuth2Client._clientId,
-                                                    clientSecret: oAuth2Client._clientSecret,
-                                                    accessToken: accessToken.token
-                                                },
-                                            });
-                                            const mailOptions = {
-                                                from: 'MC2 Serwis <zamowserwis24@gmail.om>',
-                                                to: from,
-                                                subject: `Nowe zamówienie nr ${orderID}`,
-                                                text: `Dziękujemy za złożenie zamówienia na usługę serwisową. Nr złoszenia: ${orderID}`,
-                                                html: `<h3>Dziękujemy za złożenie zamówienia na usługę serwisową. Nr złoszenia: ${orderID}</h3>`
-                                            };
-                                            const mailSent = await transport.sendMail(mailOptions);
-                                            if(mailSent.accepted){
-                                            //     await gmail.users.messages.modify({
-                                            //       userId:'me',
-                                            //       id:messages[i].id,
-                                            //       requestBody:{
-                                            //           removeLabelIds:['UNREAD']
-                                            //       }
-                                            //   })
-                                              await gmail.users.messages.delete({
-                                                userId:'me',
-                                                id:messages[i].id
-                                              })
+    // checkCredentials().then((e) => {
+    //     return new Promise((resolve, reject) => {
+    //         const gmail = google.gmail({version: 'v1',auth:e});
+    //         gmail.users.threads.list({
+    //           userId: 'me',
+    //         }, (err, res) => {
+    //           if (err) return console.log('The API returned an error: ' + err);
+    //           const messages = res.data.threads;
+    //           if (messages.length) {
+    //             for(let i=0; i<messages.length; i++){
+    //                 gmail.users.threads.get({
+    //                     userId:'me',
+    //                     id:messages[i].id
+    //                 }, (err,res)=>{
+    //                     if(err) console.log('err',err);
+    //                     const from = res.data.messages[0].payload.headers.find((h)=>h['name']==='From')
+    //                     const subject = res.data.messages[0].payload.headers.find((h)=>h['name']==='Subject');
+    //                     let text;
+    //                     if(res.data.messages[0].labelIds.includes('UNREAD')){
+    //                         const body = gmail.users.messages.get({
+    //                             userId:'me',
+    //                             id:messages[i].id
+    //                         })
+    //                         body.then(async (b)=> {
+    //                             // console.log('B',b.data.payload.parts[0].parts[0].body.data)
+    //                             console.log(b.data.payload.parts[0].hasOwnProperty('parts'))
+    //                             const data = b.data.payload.parts[0].hasOwnProperty('parts')  ? b.data.payload.parts[0].parts[0].body.data : b.data.payload.parts[0].body.data;
+    //                             text = new Buffer.from(data, 'base64').toString('utf-8');
+    //                             await mongoose.connect(process.env.MONGO_URL)
+    //                             .then(()=>console.log("DB connection Successfull"))
+    //                             .catch((error)=>console.log('MONGO ERROR',error));
+    //                             return new Promise(async (resolve, reject)=>{
+    //                                 let customer = await Customer.findOne({email:from.value});
+    //                                 if(!customer){
+    //                                     const newCustomer = new Customer({
+    //                                         email:from.value,
+    //                                         name:from.value,
+    //                                         nip:Date.now()
+    //                                     })
+    //                                     customer = await newCustomer.save();
+    //                                     console.log(customer)
+    //                                 }
+    //                                 const newOrder = new Order({
+    //                                     customer:customer._id,
+    //                                     description:text,
+    //                                     title:subject.value
+    //                                 })
+    //                                 const savedOrder = await newOrder.save();
+    //                                 console.log(savedOrder);
+    //                                 const orderID = savedOrder._id
+    //                                 resolve({from:from.value, orderID:orderID})
+    //                             }).then(async result => {
+    //                                 await new Promise(async (resolve, reject) => {
+    //                                     const { from, orderID } = result;
+    //                                     console.log('OAUTH22', oAuth2Client);
+    //                                     try {
+    //                                         const accessToken = await oAuth2Client.getAccessToken();
+    //                                         const transport = nodemailer.createTransport({
+    //                                             service: 'gmail',
+    //                                             auth: {
+    //                                                 type: 'OAuth2',
+    //                                                 user: 'zamowserwis24@gmail.com',
+    //                                                 clientId: oAuth2Client._clientId,
+    //                                                 clientSecret: oAuth2Client._clientSecret,
+    //                                                 accessToken: accessToken.token
+    //                                             },
+    //                                         });
+    //                                         const mailOptions = {
+    //                                             from: 'MC2 Serwis <zamowserwis24@gmail.om>',
+    //                                             to: from,
+    //                                             subject: `Nowe zamówienie nr ${orderID}`,
+    //                                             text: `Dziękujemy za złożenie zamówienia na usługę serwisową. Nr złoszenia: ${orderID}`,
+    //                                             html: `<h3>Dziękujemy za złożenie zamówienia na usługę serwisową. Nr złoszenia: ${orderID}</h3>`
+    //                                         };
+    //                                         const mailSent = await transport.sendMail(mailOptions);
+    //                                         if(mailSent.accepted){
+    //                                         //     await gmail.users.messages.modify({
+    //                                         //       userId:'me',
+    //                                         //       id:messages[i].id,
+    //                                         //       requestBody:{
+    //                                         //           removeLabelIds:['UNREAD']
+    //                                         //       }
+    //                                         //   })
+    //                                           await gmail.users.messages.delete({
+    //                                             userId:'me',
+    //                                             id:messages[i].id
+    //                                           })
                                                 
-                                                console.log('Mongoose disconnect');
-                                                console.log('i',i);
-                                                console.log('messages length', messages.length)
-                                                await mongoose.disconnect();
-                                                if(i === messages.length-1){
-                                                    console.log('DONE')
-                                                    response.status(200).json({mail:'confirm'})
-                                                    resolve();
-                                                }
-                                            }
-                                        } catch (error) {
-                                            console.log('error', error);
-                                        }
-                                    });
+    //                                             console.log('Mongoose disconnect');
+    //                                             console.log('i',i);
+    //                                             console.log('messages length', messages.length)
+    //                                             await mongoose.disconnect();
+    //                                             if(i === messages.length-1){
+    //                                                 console.log('DONE')
+    //                                                 response.status(200).json({mail:'confirm'})
+    //                                                 resolve();
+    //                                             }
+    //                                         }
+    //                                     } catch (error) {
+    //                                         console.log('error', error);
+    //                                     }
+    //                                 });
                                     
-                                })
-                            })
-                        }
-                    })
+    //                             })
+    //                         })
+    //                     }
+    //                 })
                     
-                    // if(i === messages.length-1){
-                    //    response.status(200).json({mail:'confirm'})
-                    // }
-                }
-                // console.log('Labels:');
-                // labels.forEach((label) => {
-                //   console.log(`- ${label.name}`);
-                // });
-              } else {
-                console.log('No labels found.');
-                reject();
-              }
-            });
-        })
-    })
-    response.status(200).json({mail:confirm})
+    //                 // if(i === messages.length-1){
+    //                 //    response.status(200).json({mail:'confirm'})
+    //                 // }
+    //             }
+    //             // console.log('Labels:');
+    //             // labels.forEach((label) => {
+    //             //   console.log(`- ${label.name}`);
+    //             // });
+    //           } else {
+    //             console.log('No labels found.');
+    //             reject();
+    //           }
+    //         });
+    //     })
+    // })
+    response.status(200).json({mail:'confirm'})
 })
 
 
